62030b58cd44318ac550c4e85cafd05137d9a053
VIKAS SHUKLA <38638875+vikas7666@users.noreply.github.com>
2024-10-08 12:41:36 +0530

    Update datatypes-summary.js

--- js-youtube/01_basic/datatypes-summary.js
+++ js-youtube/01_basic/datatypes-summary.js
@@ -1,33 +1,33 @@
 // Jaavascript data type is basically two type 
-/*
-Primitive (7 Types)  
+
+//Primitive (7 Types)  
     1) string 
     2) Number
     3) Boolean
     4) null   // empty
     5) undefined  // variable declared memory space declared not but not assigned 
-    6) symboal ==> kisi bhi value ko unique banane ke liye use hotaa hai 
+    6) symbol ==> kisi bhi value ko unique banane ke liye use hotaa hai 
     7) BigInt
 
-Non Primitive (Reference Type)
+//Non Primitive (Reference Type)
     1) Array
     2) Object
     3) Function
 
-    Javascript is dyanmically type language or statically type language 
+    //Javascript is dyanmically type language or statically type language 
 
     const score = false;
-    cons temprature =  null ==> don't have any temparture 
+    cons temprature =  null // ==> don't have any temparture 
     const userEmail; udefined 
-*/
+
 
 const id = Symbol('123'); // Symbol 123 symbol always provide unique value even if you initialize same value
 // const anotherID = Symbol('123')
 // console.log(id === anotherID); // false
 
-/* 
-Return type of variables in JavaScript
-1) Primitive Datatypes
+
+//Return type of variables in JavaScript
+//1) Primitive Datatypes
        Number => number
        String  => string
        Boolean  => boolean
@@ -36,11 +36,11 @@ Return type of variables in JavaScript
        Symbol  =>  symbol
        BigInt  =>  bigint
 
-2) Non-primitive Datatypes
+//2) Non-primitive Datatypes
        Arrays  =>  object
        Function  =>  function  ==> object function
        Object  =>  object
-*/
+
 
 let array1 = ['hello']
 console.log(typeof array1); // Object
@@ -51,13 +51,10 @@ let myfunction = function abc() {
 
 // ************************  Memory ************************************
 
-/* 
-    1) Stack  (Primitive)   ==>  Copy
-    2) Heap   (Non Primitive type)   ==> reference ==> original value
-*/
+//  1) Stack  (Primitive)   ==>  Copy
+ // 2) Heap   (Non Primitive type)   ==> reference ==> original value
 
 let myYoutube = 'vikasshukla.in'
-
 let anotherYoutube = myYoutube;
 anotherYoutube  = 'nonYoutube'
 
